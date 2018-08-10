fd419a8357e0e69f6cd060767232c9ddd97d68cd
dapengyou <ladyztt@sina.com>
2018-08-10 10:02:47 +0800

    添加证书，多渠道打包，拷贝apk至指定目录

--- app/src/main/java/com/test/aidl/MainActivity.java
+++ app/src/main/java/com/test/aidl/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
